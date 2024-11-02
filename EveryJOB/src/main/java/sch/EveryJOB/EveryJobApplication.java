d35deed230d238162a57458b9d102fc0cc71873b
DongUk <az9170@naver.com>
2024-11-02 16:31:40 +0900

    feat: 프로젝트 생성

---
+++ EveryJOB/src/main/java/sch/EveryJOB/EveryJobApplication.java
@@ -0,0 +1,13 @@
+package sch.EveryJOB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryJobApplication.class, args);
+	}
+
+}
