fe80cdc1aabaca67fb0e2f7ea6f56fc8e5536ee9
DongUk <az9170@naver.com>
2024-11-04 22:49:01 +0900

    feat: 멤버 에러 코드 추가

---
+++ EveryJOB/src/main/java/sch/EveryJOB/common/error/MemberErrorCode.java
@@ -0,0 +1,19 @@
+package sch.EveryJOB.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum MemberErrorCode implements ErrorCodeIfs {
+    
+    
+    MEMBER_NOT_FOUND(400, "멤버를 찾을 수 없음"),
+    MEMBER_ALREADY_EXIST(400, "멤버가 이미 존재함"),
+    MEMBER_WRONG_PASSWORD(400, "비밀번호 불일치"),
+    MEMBER_SESSION_ERROR(400, "세션 오류")
+    ;
+    
+    private final Integer httpStatusCode;
+    private final String errorDescription;
+}
